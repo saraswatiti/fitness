f1ec7981529270e107b9e41ee95b4687e5ac5801
saraswatiti <saraswati.rigorousweb@gmail.com>
2020-10-14 18:15:13 +0545

    Updates

---
+++ precache-manifest.3feb60852d02329b36a5240a8a7e48c5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3439d21a85e5e67fc1f4509ef7c41fe3",
+    "url": "/fitness/index.html"
+  },
+  {
+    "revision": "dc24306c173517bf674a",
+    "url": "/fitness/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c9f7c3ca5d03152118ce",
+    "url": "/fitness/static/css/main.d5fea41d.chunk.css"
+  },
+  {
+    "revision": "dc24306c173517bf674a",
+    "url": "/fitness/static/js/2.e8599f03.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fitness/static/js/2.e8599f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b33c598a91c43e5afe",
+    "url": "/fitness/static/js/3.e6086e2a.chunk.js"
+  },
+  {
+    "revision": "c9f7c3ca5d03152118ce",
+    "url": "/fitness/static/js/main.ad47d102.chunk.js"
+  },
+  {
+    "revision": "24fbc357385330a820ef",
+    "url": "/fitness/static/js/runtime-main.64baa5b8.js"
+  },
+  {
+    "revision": "31cb05f3e1c4697812b2104049d21c55",
+    "url": "/fitness/static/media/contact-bg.31cb05f3.jpg"
+  },
+  {
+    "revision": "f042c2ac7fe16211f6c2806e04fdebad",
+    "url": "/fitness/static/media/cta-bg.f042c2ac.jpg"
+  },
+  {
+    "revision": "7c2ab25d46f7a6f96ce234d934f56a80",
+    "url": "/fitness/static/media/first-trainer.7c2ab25d.jpg"
+  },
+  {
+    "revision": "ae2508775e7b40b7f9678a27610e3578",
+    "url": "/fitness/static/media/gym-video.ae250877.mp4"
+  },
+  {
+    "revision": "02e364823438fedfc552123239968bf6",
+    "url": "/fitness/static/media/schedule-bg.02e36482.jpg"
+  },
+  {
+    "revision": "db6c16448e4272b3e4eb88cdc84c356f",
+    "url": "/fitness/static/media/second-trainer.db6c1644.jpg"
+  },
+  {
+    "revision": "0a63076277a6405d22883465bbcfad43",
+    "url": "/fitness/static/media/slide-01.0a630762.jpg"
+  },
+  {
+    "revision": "81e56a6dd10134e1affed0e5f247541f",
+    "url": "/fitness/static/media/third-trainer.81e56a6d.jpg"
+  },
+  {
+    "revision": "5a6c841c01f957d21afe705bf1754ce0",
+    "url": "/fitness/static/media/training-image-01.5a6c841c.jpg"
+  },
+  {
+    "revision": "a6ebee97d6a29927f91b2a238d1b8336",
+    "url": "/fitness/static/media/training-image-02.a6ebee97.jpg"
+  },
+  {
+    "revision": "3d605f688128d539f1898c2c8867814b",
+    "url": "/fitness/static/media/training-image-03.3d605f68.jpg"
+  },
+  {
+    "revision": "dd1df8f478bb28433b5dfc6df4832f09",
+    "url": "/fitness/static/media/training-image-04.dd1df8f4.jpg"
+  }
+]);
